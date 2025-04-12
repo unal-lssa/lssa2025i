83f4f1bad2e6a1af03b4df920e5ddc1fca45cc0c
Daniel Rincon <66699650+Danijandro03@users.noreply.github.com>
2025-04-12 18:56:48 -0500

    Add files via upload

---
+++ laboratories/laboratory_2/1018486757/generation.py
@@ -0,0 +1,6 @@
+from metamodel import create_metamodel 
+from transformations import apply_transformations 
+if __name__ == '__main__': 
+    metamodel = create_metamodel() 
+    model = metamodel.model_from_file('model.arch')
+    apply_transformations(model) 
