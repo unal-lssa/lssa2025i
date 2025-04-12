eb57d51ec4406fe9db0d648aeaeaf7f4514a33a3
jhunter5 <jhunter@unal.edu.co>
2025-04-12 11:14:12 -0500

    refactor: reorganize directory structure to match delivery format

--- laboratories/laboratory_2/generation.py
+++
@@ -1,9 +0,0 @@
-from metamodel import create_metamodel
-from transformations import apply_transformations
-
-if __name__ == '__main__':
-    metamodel = create_metamodel()
-    model = metamodel.model_from_file('model.arch')
-    apply_transformations(model)
-
-    
