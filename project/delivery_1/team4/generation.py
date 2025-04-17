75b8da6114123848e5d1551092fd452401058aed
ssuarezs <suarez.santiago.113@gmail.com>
2025-04-17 12:50:46 -0500

    team 4 - move to subfolder -team4-

--- project/delivery_1/team4/generation.py
+++ project/delivery_1/team4/generation.py
@@ -1,3 +1,4 @@
+import os
 from metamodel import create_metamodel
 from textx.export import metamodel_export, model_export
 
@@ -5,6 +6,7 @@ if __name__ == '__main__':
     metamodel = create_metamodel()
     model = metamodel.model_from_file('model.arch')
 
-    # Generate Metamodel and Model diagrams
+    # Generate Metamodel and Model diagrams (.dot format)
+    os.makedirs('diagrams', exist_ok=True)
     metamodel_export(metamodel, 'diagrams/metamodel.dot')
     model_export(model, 'diagrams/model.dot')
