563710138c141d53ee66972998a177a131f229d8
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2025-04-28 17:22:30 -0500

    Feat: Setup the structure for the docker compose transformation

--- project/delivery_1/team4/transformations/dockercompose.py
+++ project/delivery_1/team4/transformations/dockercompose.py
@@ -1,4 +1,18 @@
-from transformations.templates import _make_component_dir
+from transformations.templates import generate_templated_component 
 
-def generate_docker_compose(components):
-    path = _make_component_dir("")
+
+def generate_docker_compose(model):
+    """Generate the docker compose for the system"""
+
+    params = {
+        "name": getattr(model, "name", "System Architecture"),
+        "components": [
+            {
+                "name": component.name,
+            }
+            for component in model.elements
+            if component.__class__.__name__ == "Component"
+        ],
+    }
+
+    generate_templated_component(name="", template_name="docker-compose", params=params)
