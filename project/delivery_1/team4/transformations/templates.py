9243c31c76525be090bee831980335dbdf1d7d1c
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2025-04-28 00:22:46 -0500

    Feat: :sparkles: Update templating logic
    Use Jinja2 as templating engine for allowing for easier and more
    powerful template management.

--- project/delivery_1/team4/transformations/templates.py
+++ project/delivery_1/team4/transformations/templates.py
@@ -1,47 +1,59 @@
 from pathlib import Path
 
+import jinja2
+
 PROJECT_BASE = Path.cwd() / "skeleton"
 TEMPLATES_BASE = Path.cwd() / "templates"
 
 
-# Create the location for a given component
-def _make_component_dir(component_name: str, project_base: Path = PROJECT_BASE) -> Path:
-    path = project_base / component_name
+def _make_dir(dir_name: str, dir_path: Path) -> Path:
+    """Create the location for a given component"""
+
+    path = dir_path / dir_name
     path.mkdir(parents=True, exist_ok=True)
     return path
 
 
-# Make a copy of the files in a template directory to the target location and replace
-# the parameters in templates.
-
 def _copy_template(
     template_path: Path, target_path: Path, params: dict | None = None
 ) -> None:
-    for template in template_path.rglob("*"):
-        if template.is_file():
+    """
+    Make a copy of the files in a template directory to the target location and replace
+    the parameters in templates.
+    """
 
-            relative_path = template.relative_to(template_path)
-            target_file = target_path / relative_path
+    jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(template_path),
+    )
 
-            target_file.parent.mkdir(parents=True, exist_ok=True)
+    for item in template_path.iterdir():
+        # If the item is a directory, create the corresponding directory in the target path
+        if item.is_dir():
+            new_target = _make_dir(item.name, target_path)
+            # Recursively copy the contents of the directory
+            _copy_template(item, new_target, params)
 
-            with template.open("r") as template_file:
-                content = template_file.read()
+        elif item.is_file():
+            # If the item is a file, process it as a template
+            template = jinja_env.get_template(item.name)
+            target_file_path = target_path / item.stem
+            with target_file_path.open("w") as target_file:
+                target_file.write(template.render(**params))
 
-            if params is not None:
-                for param, value in params.items():
-                    content = content.replace(f"{{{{{param}}}}}", value)
-
-            with (target_file).open("w") as target_file:
-                target_file.write(content)
-
-
-# A templated component is that for which the only logic in generating is
-# copying the files of a template and replacing some params in the templates.
 def generate_templated_component(
-    name: str, template_name: str, params: dict = None
+    name: str,
+    template_name: str,
+    params: dict | None = None,
 ) -> None:
-    path = _make_component_dir(name)
+    """Generate a templated component
+    A templated component is that for which the only logic in generating is
+    copying the files of a template and replacing some params in the templates.
+    """
+    if params is None:
+        params = {}
+
+
+    path = _make_dir(name, PROJECT_BASE)
     _copy_template(
         template_path=TEMPLATES_BASE / template_name, target_path=path, params=params
     )
