44a23017d70f81c54e87f04c85ec2f454cc24604
JuanAlcina <micorreosar@gmail.com>
2025-05-01 18:20:57 -0500

    Quick change

--- laboratories/laboratory_4/1020814023/load_test.py
+++ laboratories/laboratory_4/1020814023/load_test.py
@@ -2,7 +2,6 @@ import asyncio
 import aiohttp
 import time
 
-BASE_URL = "http://127.0.0.1:5000"  # Updated to match your Postman URL
 USERNAME = "user1"
 PASSWORD = "password123"
 TOTAL_REQUESTS = 100
@@ -12,14 +11,14 @@ TEST_DATA_ENDPOINT = True  # Set to True to test /data endpoint
 sem = asyncio.Semaphore(CONCURRENT)
 
 async def get_token(session):
-    url = f"{BASE_URL}/login"
+    url = "http://127.0.0.1:5000/login"
     async with session.post(url, json={"username": USERNAME, "password": PASSWORD}) as resp:
         data = await resp.json()
         return data.get("token")
 
 async def send_request(session, token, i):
     headers = {"Authorization": f"{token}"}  # Correct format of the Authorization header
-    url = f"{BASE_URL}/data" if TEST_DATA_ENDPOINT else f"{BASE_URL}/longtask"
+    url = "http://127.0.0.1:5000/data" if TEST_DATA_ENDPOINT else f"http://127.0.0.1:5000/longtask"
     
     async with sem:
         try:
