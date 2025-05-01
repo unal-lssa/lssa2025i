bf2a0ce691f5aff9f982b27dd5759152dc3e4288
Mario Montero <marioupc3@gmail.com>
2025-05-01 10:21:54 -0500

    Add laboratory 4

---
+++ laboratories/laboratory_4/1065833411/user_service/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_users():
+    with open("user_db.json") as f:
+        data = json.load(f)
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
