b703c8b38c0b6b6ba1ade0538f7724402557c84b
camilonfs1 <camevargasrom@unal.edu.co>
2025-05-02 20:32:51 -0500

    update laboratory 5

--- laboratories/laboratory_5/main_simulator.py
+++
@@ -1,6 +0,0 @@
-from simulator.simulator import main
-from simulator.repot_generator import generate_status_report_pdf
-
-if __name__ == "__main__":
-    main()
-    generate_status_report_pdf("simulator/status_log.json")
