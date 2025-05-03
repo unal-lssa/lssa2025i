933c58db46f1fae367172dd1d74e50696f1a88e6
ssuarezs <suarez.santiago.113@gmail.com>
2025-05-02 22:06:13 -0500

    Lab 4 - 1001326848 - Improve testing - using 2 diferent routes

--- laboratories/laboratory_4/1001326848/apps/microservice/app.py
+++ laboratories/laboratory_4/1001326848/apps/microservice/app.py
@@ -1,17 +1,15 @@
 from flask import Flask, jsonify
+import time
 
 app = Flask(__name__)
 
 
 @app.route("/process", methods=["GET"])
 def process():
+    # Simulate 50 ms latency
+    time.sleep(0.05)
     return jsonify({"message": "Business logic executed"}), 200
 
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=False,
-        threaded=True
-    )
+    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
