51df03b521006c8a1b54258c5b97fdf696105204
Carlos Sandoval <csandovalc@unal.edu.co>
2025-05-07 20:56:06 -0500

    feat: implement architecture element conversion and generation logic
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

---
+++ project/delivery_1/refactor/README.md
@@ -0,0 +1,10 @@
+# Project
+
+## Authors
+
+- Juan Andres Orozco Velandia
+- Juan Esteban Hunter Malaver
+- Carlos Santiago Sandoval Casallas
+- Camilo Andres Cuello Romero
+- Lucas Peña Salas
+- Santiago Restrepo
--- project/delivery_1/refactor/src/Generation/transformations.py
+++ project/delivery_1/refactor/src/Generation/transformations.py
@@ -1,49 +1,107 @@
-import os, yaml
-from ..DSL.IVisitor import IVisitor
-from ..DSL.Model import Model
-from ..DSL.StandardComponent import StandardComponent
-from ..DSL.Database import Database
-from ..DSL.Queue import Queue
-from ..DSL.LoadBalancer import LoadBalancer
-from ..DSL.ApiGateway import ApiGateway
-from ..DSL.Network import Network
-from ..DSL.Connector import Connector
 from ..DSL.AComponent import AComponent
-from typing import Optional
+from ..DSL.ApiGateway import ApiGateway
+from ..DSL.Connector import Connector, ConnectorType
+from ..DSL.Database import Database, DatabaseType
+from ..DSL.IElement import IElement
+from ..DSL.IVisitor import IVisitor
+from ..DSL.LoadBalancer import LoadBalancer
+from ..DSL.Model import Model
+from ..DSL.Network import Network
+from ..DSL.Queue import Queue
+from ..DSL.StandardComponent import StandardComponent, StandardComponentType
+
+# Change the visitor to change the deployment model logic
+from .DockerWriterVisitor import DockerWriterVisitor
+from .CodeGeneratorVisitor import CodeGeneratorVisitor
 
 
-class DockerWriterVisitor(IVisitor):
-    def __init__(self, output_dir: str = "skeleton"):
-        self._services = {}
-        self._output = output_dir
-        os.makedirs(self._output, exist_ok=True)
+def convert_textx_to_dsl_element(elem) -> IElement:
+    """
+    Converts a TextX element to a DSL element.
+    """
+    cls_name = elem.__class__.__name__
+    if cls_name == "Network":
+        return Network(
+            name=elem.name,
+            internal=getattr(elem, "internal", False),
+            driver=getattr(elem, "driver", None),
+        )
+    if cls_name == "StandardComponent":
+        comp_type = StandardComponentType(elem.type)
+        network = convert_textx_to_dsl_element(elem.network)
+        return StandardComponent(name=elem.name, comp_type=comp_type, network=network)
+    if cls_name == "Database":
+        db_type = DatabaseType(elem.databaseType)
+        network = convert_textx_to_dsl_element(elem.network)
+        return Database(name=elem.name, database_type=db_type, network=network)
+    if cls_name == "LoadBalancer":
+        # target is a reference to another TextX component
+        target = convert_textx_to_dsl_element(elem.target)
+        network = convert_textx_to_dsl_element(elem.network)
+        return LoadBalancer(
+            name=elem.name,
+            instance_count=elem.instanceCount,
+            target=target,
+            network=network,
+        )
+    if cls_name == "ApiGateway":
+        auth = convert_textx_to_dsl_element(elem.auth)
+        networks = [convert_textx_to_dsl_element(n) for n in elem.networks]
+        return ApiGateway(name=elem.name, auth=auth, networks=networks)
+    if cls_name == "Queue":
+        network = convert_textx_to_dsl_element(elem.network)
+        return Queue(
+            name=elem.name,
+            partitions=elem.partitions,
+            replication=elem.replication,
+            network=network,
+        )
+    if cls_name == "Connector":
+        source = convert_textx_to_dsl_element(
+            elem["from"]
+        )  # 'from' is a reserved keyword
+        target = convert_textx_to_dsl_element(elem.to)
+        conn_type = ConnectorType(elem.type)
+        return Connector(from_comp=source, to_comp=target, conn_type=conn_type)
+    raise ValueError(f"Unsupported element type: {cls_name}")
 
-    def visit_model(self, model: Model):
-        pass
 
-    def visit_standard_component(self, comp: StandardComponent) -> None:
-        self._write_service(comp, image=None)
+def generate_architecture(model, output_dir: str = "skeleton"):
+    converted_elems = []
+    textx_to_domain_map = {}
 
-    def visit_database(self, db: Database) -> None:
-        self._write_service(db, image=None)
+    # First create all network elements to resolve references
+    for elem in model.elements:
+        if elem.__class__.__name__ == "Network":
+            net = convert_textx_to_dsl_element(elem)
+            textx_to_domain_map[elem] = net
+            converted_elems.append(net)
 
-    def visit_queue(self, q: Queue) -> None:
-        self._write_service(q, image=None)
+    # Then create all other elements (except Connectors), using the network elements created above
+    for elem in model.elements:
+        if (
+            elem.__class__.__name__ != "Network"
+            and elem.__class__.__name__ != "Connector"
+        ):
+            inst = convert_textx_to_dsl_element(elem)
+            textx_to_domain_map[elem] = inst
+            converted_elems.append(inst)
 
-    def visit_load_balancer(self, lb: LoadBalancer) -> None:
-        self._write_service(lb, image=None)
+    # Finally create all Connector elements, using the network and other elements created above
+    for elem in model.elements:
+        if elem.__class__.__name__ == "Connector":
+            conn = convert_textx_to_dsl_element(elem)
+            textx_to_domain_map[elem] = conn
+            converted_elems.append(conn)
 
-    def visit_api_gateway(self, ag: ApiGateway) -> None:
-        self._write_service(ag, image=None)
+    # Validate the architecture
+    arch = Model(
+        elements=converted_elems,
+    )
+    arch.validate()
 
-    def visit_network(self, network: Network) -> None:
-        pass
-
-    def visit_connector(self, conn: Connector) -> None:
-        pass
-
-    def post_visit_model(self, model: Model) -> None:
-        pass
-
-    def _write_service(self, comp: AComponent, image: Optional[str]) -> None:
-        pass
+    # Use the visitor to generate the architecture
+    docker_visitor = DockerWriterVisitor(output_dir)
+    code_visitor = CodeGeneratorVisitor(output_dir)
+    arch.accept(docker_visitor)
+    arch.accept(code_visitor)
